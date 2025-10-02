1f26385cb503411c1b1583c8bb48ff70c86fbd08
unknown <jeonjy99@naver.com>
2025-10-02 13:35:34 +0900

    fix:loading modal

--- frontend_memo_app/src/App.vue
+++ frontend_memo_app/src/App.vue
@@ -416,6 +416,7 @@ export default {
       pendingSubmission,
       correctAnswerModalOpen,
       correctAnswerData,
+      isVerifying,
       handleProblemSelect,
       handleAddImageToCanvas,
       handleSubmitAnswer,
